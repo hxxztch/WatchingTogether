8a9983e0ecec35835adfc309e8445d156139935e
hxxztch <l3400532974@163.com>
2026-08-03 00:14:15 +0800

    添加crash.log崩溃日志系统

--- client.py
+++ client.py
@@ -21,6 +21,27 @@ else:
         os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = os.path.abspath(_plugins_dir)
 
 os.environ['QT_LOGGING_RULES'] = 'qt.qpa.mousegrab.warning=false'
+
+
+import logging, datetime, traceback
+
+# ---------- crash logger ----------
+def _setup_crash_log():
+    log_dir = os.path.dirname(sys.executable) if getattr(sys, "frozen", False) else _script_dir
+    log_path = os.path.join(log_dir, "crash.log")
+    fh = logging.FileHandler(log_path, encoding="utf-8")
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(logging.Formatter("[%%(asctime)s] %(levelname)s: %(message)s"))
+    logging.getLogger().addHandler(fh)
+    logging.getLogger().setLevel(logging.DEBUG)
+    logging.info("Client starting (frozen=%s)", getattr(sys, "frozen", False))
+    # Global exception hook
+    def _excepthook(exc_type, exc_val, tb):
+        logging.critical("".join(traceback.format_exception(exc_type, exc_val, tb)))
+        sys.__excepthook__(exc_type, exc_val, tb)
+    sys.excepthook = _excepthook
+
+_setup_crash_log()
 from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QIcon, QPixmap
 from app.ui.main_window import MainWindow
---
+++ 超时空会夜机.spec
@@ -0,0 +1,45 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+
+a = Analysis(
+    ['client.py'],
+    pathex=[],
+    binaries=[],
+    datas=[('app', 'app'), ('assets', 'assets')],
+    hiddenimports=['PySide6', 'mpv', 'websockets'],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    noarchive=False,
+    optimize=0,
+)
+pyz = PYZ(a.pure)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    [],
+    exclude_binaries=True,
+    name='超时空会夜机',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    console=False,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+    icon=['assets\\app_icon.ico'],
+)
+coll = COLLECT(
+    exe,
+    a.binaries,
+    a.datas,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    name='超时空会夜机',
+)
